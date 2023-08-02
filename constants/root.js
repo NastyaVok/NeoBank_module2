92e6bc3a126ed1d6dcc557d183cb5a133047e5ea
Nastya <Nastkoval97@yandex.ru>
2023-08-02 16:31:13 +0300

    Updates

--- constants/root.js
+++
@@ -1,8 +0,0 @@
-export const ROOT_HEADER = document.getElementById('header');
-export const ROOT_CARDS = document.getElementById('cards');
-export const ROOT_FEATURES = document.getElementById('features');
-export const ROOT_EXCHANGE = document.getElementById('exchange');
-export const ROOT_SERVICES = document.getElementById('services');
-export const ROOT_NEWS = document.getElementById('news');
-export const ROOT_SUPPORT = document.getElementById('support');
-export const ROOT_FOOTER = document.getElementById('footer');
--- index.js
+++
@@ -1,7 +0,0 @@
-import App from './components/App'
-import Exchange from './components/Exchange'
-
-(async () => {
-	await App.render()
-	await setInterval(Exchange.render, 900000)
-})()
